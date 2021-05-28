2c952a816422e4cf47504102ddfc5096c306d70a
Sérgio Cardoso <sergioifbaiano@hotmail.com>
2021-05-27 23:31:33 -0300

    Swagger

--- src/main/java/br/com/polar/biblioteca/controller/ClientController.java
+++ src/main/java/br/com/polar/biblioteca/controller/ClientController.java
@@ -17,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.polar.biblioteca.entity.Client;
 import br.com.polar.biblioteca.service.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/clients")
+@Api ( value = "onlinestore" , description = "Controle dos serviços de cliente" )
 public class ClientController {
 	
 	@Autowired
@@ -29,26 +34,66 @@ public class ClientController {
 	public ResponseEntity<List<Client>> findAll() {
 		return ResponseEntity.status(HttpStatus.OK).body(clientService.findAll());
 	} */
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}",produces="application/json")
+	@ApiOperation ( value = "Buscar cliente por ID" )
+	@ApiResponses ( value = {
+	        @ApiResponse ( code = 200 , message = "Lista recuperada com sucesso" ) ,
+	        @ApiResponse ( code = 401 , message = "Você não está autorizado a visualizar o recurso" ) ,
+	        @ApiResponse ( code = 403 , message = "O acesso ao recurso que você estava tentando acessar é proibido" ) ,
+	        @ApiResponse ( code = 404 , message = "O recurso que você estava tentando acessar não foi encontrado" )
+	}
+	)
 	public ResponseEntity<Client> findById(@PathVariable (value = "id")Long id) {
 		return ResponseEntity.status(HttpStatus.OK).body(clientService.findById(id));
 	}
-	@GetMapping
+	@GetMapping(produces="application/json")
+	@ApiOperation ( value = "Buscar cliente por nome" )
+	@ApiResponses ( value = {
+	        @ApiResponse ( code = 200 , message = "Lista recuperada com sucesso" ) ,
+	        @ApiResponse ( code = 401 , message = "Você não está autorizado a visualizar o recurso" ) ,
+	        @ApiResponse ( code = 403 , message = "O acesso ao recurso que você estava tentando acessar é proibido" ) ,
+	        @ApiResponse ( code = 404 , message = "O recurso que você estava tentando acessar não foi encontrado" )
+	}
+	)
 	public ResponseEntity<List<Client>> findByName(@RequestParam String name) {
 		return ResponseEntity.status(HttpStatus.OK).body(clientService.findByName(name));
 	}
 	
-	@PostMapping
+	@PostMapping(produces="application/json", consumes="application/json")
+	@ApiOperation ( value = "Criar cliente" )
+	@ApiResponses ( value = {
+	        @ApiResponse ( code = 201 , message = "Lista recuperada com sucesso" ) ,
+	        @ApiResponse ( code = 401 , message = "Você não está autorizado a visualizar o recurso" ) ,
+	        @ApiResponse ( code = 403 , message = "O acesso ao recurso que você estava tentando acessar é proibido" ) ,
+	        @ApiResponse ( code = 404 , message = "O recurso que você estava tentando acessar não foi encontrado" )
+	}
+	)
 	public ResponseEntity<Client> create(@RequestBody Client client) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(clientService.create(client));
 	}
 	
-	@PutMapping
+	@PutMapping(produces="application/json", consumes="application/json")
+	@ApiOperation ( value = "Atualizar cliente" )
+	@ApiResponses ( value = {
+	        @ApiResponse ( code = 200 , message = "Lista recuperada com sucesso" ) ,
+	        @ApiResponse ( code = 401 , message = "Você não está autorizado a visualizar o recurso" ) ,
+	        @ApiResponse ( code = 403 , message = "O acesso ao recurso que você estava tentando acessar é proibido" ) ,
+	        @ApiResponse ( code = 404 , message = "O recurso que você estava tentando acessar não foi encontrado" )
+	}
+	)
 	public ResponseEntity<Client> update(@RequestBody Client client) {
 		return ResponseEntity.status(HttpStatus.OK).body(clientService.update(client));
 	}
 	
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "/{id}",produces="application/json", consumes="application/json")
+	@ApiOperation ( value = "Deletar cliente" )
+	@ApiResponses ( value = {
+	        @ApiResponse ( code = 200 , message = "Lista recuperada com sucesso" ) ,
+	        @ApiResponse ( code = 401 , message = "Você não está autorizado a visualizar o recurso" ) ,
+	        @ApiResponse ( code = 403 , message = "O acesso ao recurso que você estava tentando acessar é proibido" ) ,
+	        @ApiResponse ( code = 404 , message = "O recurso que você estava tentando acessar não foi encontrado" )
+	}
+	)
 	public ResponseEntity<Client> delete(@PathVariable (value = "id")Long id) {
 		clientService.delete(id);
 		return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/br/com/polar/biblioteca/entity/Client.java
+++ src/main/java/br/com/polar/biblioteca/entity/Client.java
@@ -6,12 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
+@Api (description = "Clientes da biblioteca" )
 public class Client {
 	@javax.persistence.Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(value = "Código único do cliente",example = "1")
 	private Long id;
+	@ApiModelProperty(value = "nome do cliente", example = "maria jose")
 	private String name;
+	@ApiModelProperty(value = "data de nascimento do cliente",example = "2020-02-02")
 	private LocalDate birthDate;
 	
 	public Long getId() {
